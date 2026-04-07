0041e1ac2aec90001d48eea845226975e98c2b8d
kajaviousmontoya <montoyakajavious@gmail.com>
2026-04-07 12:09:40 -0700

    I printed 9 more updates from the start

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String favoriteColor = "Blue";
+        System.out.println(favoriteColor);
+        int thisYear = 2026;
+        System.out.println(thisYear);
+        char midInitial = 's';
+        System.out.println(midInitial);
+        boolean hasPet = false;
+        System.out.println(hasPet);
+        String quote = "We got this!";
+        System.out.println(quote);
+        int daysOfweek = 7;
+        System.out.println(daysOfweek);
+        double coffeePrice = 4.99;
+        System.out.println(coffeePrice);
+        char favLetter = 'k';
+        System.out.println(favLetter);
+        boolean currentlyRaining = false;
+        System.out.println(currentlyRaining);
+
+
+
+
+
+
+
+
+
+    }
+}
