43a0072af361925b4bd21e62fccf2bef46365ef0
kajaviousmontoya <montoyakajavious@gmail.com>
2026-04-08 07:06:23 -0700

    I made 2 string scenarios and also printed those two scenarios

---
+++ src/main/java/com/pluralsight/reciept.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class reciept {
+    public static void main(String[] args) {
+
+        String itemName = "Sneakers";
+        double itemPrice = 150.99;
+        int quantity = 2;
+        System.out.println("You bought" + 2 + itemName + " for " + itemPrice);
+        String itemName = "apples";
+        double itemPrice = 7.10;
+        int quantity = 3;
+        System.out.println("You bought" + 3 + itemName + " for " + itemPrice);
+
+
+
+
+    }
+}
